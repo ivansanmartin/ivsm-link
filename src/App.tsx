d3400bde38192c6192e74a4dd73b629b2ee45978
ivansanmartin <ivansanmartin987@gmail.com>
2025-03-15 01:08:31 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,52 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+
+  interface DataType {
+    short_url: string
+  }
+
+  interface ApiResponse {
+    ok?: boolean;
+    message?: string;
+    data?: DataType;
+  }
+
+  const [inputValue, setInputValue] = useState("");
+  const [responseApi, setResponseApi] = useState<ApiResponse | null>(null);
+  const [isLoading, setLoading] = useState(false);
+
+  const handleInputChange = (event: any) => {
+    setInputValue(event.target.value);
+  }
+
+  const onClickButtonShort = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    try {
+      setLoading(true);
+      const response = await fetch("http://localhost:8080/api/v1/url-shortener", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-API-Key": import.meta.env.VITE_URL_SHORTENER_API_KEY
+        },
+        body: JSON.stringify({ "original_url": inputValue })
+      });
+  
+      if (!response.ok) {
+        throw new Error("Error en la solicitud");
+      }
+  
+      const data = await response.json();
+      setResponseApi(data);
+      setLoading(false)
+    } catch (error) {
+      console.error("Error al acortar la URL:", error);
+    }
+  };
+
 
   return (
     <>
@@ -9,13 +55,13 @@ function App() {
           <div className="col-lg-8 col-md-10">
             
             <div className="text-center mb-5">
-              <h1 className="display-4 fw-bold text-primary">LinkShort</h1>
+              <h1 className="display-4 fw-bold text-primary">Ivsm-Link | URL-Shortener</h1>
               <p className="lead text-muted">Shorten your links in seconds</p>
             </div>
             
             <div className="card shadow-sm border-0 rounded-4">
               <div className="card-body p-4">
-                <form>
+              <form onSubmit={onClickButtonShort}>
                   <div className="mb-3">
                     <label htmlFor="originalUrl" className="form-label visually-hidden">Your long URL</label>
                     <div className="input-group">
@@ -28,6 +74,8 @@ function App() {
                         id="originalUrl" 
                         placeholder="Paste your long URL here" 
                         required
+                        value={inputValue}
+                        onChange={handleInputChange}
                         autoComplete="off" /
                       >
                     </div>
@@ -36,27 +84,49 @@ function App() {
                     <button type="submit" className="btn btn-primary btn-lg rounded-3">
                       Shorten
                     </button>
+
+                    {
+                      inputValue.length == 0 && (
+                        <div className='text-danger mt-4'>* Please, paste your long URL.</div>
+                      )
+                    }
+
                   </div>
                 </form>
               </div>
             </div>
-            
-            <div className="card mt-4 shadow-sm border-0 rounded-4" id="resultsCard">
-              <div className="card-body p-4">
-                <h5 className="card-title mb-3">Your shortened link</h5>
-                <div className="input-group mb-3">
-                  <input type="text" className="form-control bg-light" id="shortUrl" readOnly value="https://shrtn.kr/a1b2c3" />
-                  <button className="btn btn-outline-primary" type="button" id="copyButton">
-                    Copy
-                  </button>
+
+
+            {
+              isLoading && (
+                <div className='d-flex w-100 justify-content-center gap-3 mt-4'>
+                  <div className="spinner-border text-primary" role="status">
+                  </div>
+                  <span>Generating short URL...</span>
                 </div>
-                <div className="d-flex justify-content-between align-items-center">
-                  <span className="text-muted small">Created just now</span>
-                  <a href="#" className="btn btn-sm btn-light">Create new</a>
+              )
+            }
+            
+            {
+              responseApi?.ok && 
+              <div className="card mt-4 shadow-sm border-0 rounded-4" id="resultsCard">
+                <div className="card-body p-4">
+                  <h5 className="card-title mb-3">Your shortened link</h5>
+                  <div className="input-group mb-3">
+                    <input type="text" className="form-control bg-light" id="shortUrl" readOnly value={responseApi?.data?.short_url} />
+                    <button className="btn btn-outline-primary" type="button" id="copyButton">
+                      Copy
+                    </button>
+                  </div>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <span className="text-muted small">Created just now</span>
+                    <a href="#" className="btn btn-sm btn-light">Create new</a>
+                  </div>
                 </div>
               </div>
-            </div>
             
+            }
+
             <div className="row row-cols-1 row-cols-md-3 g-4 mt-4 text-center">
               <div className="col">
                 <div className="p-3">
