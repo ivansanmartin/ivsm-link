3757a84131b7c96d95bae384edd6157b4a44b166
ivansanmartin <ivansanmartin987@gmail.com>
2025-03-16 22:01:42 -0300

    test

---
+++ backend/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+origins = [
+    "https://ivsm.link",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
